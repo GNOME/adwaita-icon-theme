f40018dc3026ce0a433f7d97c97bb3c58d8be462
Rodney Dawes <dobey@novell.com>
2005-07-15 00:13:39 +0000

    Updated for release
    2005-07-14  Rodney Dawes  <dobey@novell.com>
    
    	* NEWS: Updated for release
    
    	* autogen.sh: Require automake 1.9
    
    	* configure.in: Up version to 2.11.5
    	Require automake 1.9 and use tar-ustar

--- autogen.sh
+++ autogen.sh
@@ -5,7 +5,7 @@ srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
 PKG_NAME="gnome-icon-theme"
-REQUIRED_AUTOMAKE_VERSION=1.6
+REQUIRED_AUTOMAKE_VERSION=1.9
 
 (test -f $srcdir/configure.in \
   && test -f $srcdir/index.theme.in \
