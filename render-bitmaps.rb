a4bffe212b9cad8b9920f02130ed5cd174c9a26e
Jakub Steiner <jimmac@gmail.com>
2009-04-21 13:33:58 +0200

    move all our one-canvas plates in the one-canvas branch

--- render-bitmaps.rb
+++ render-bitmaps.rb
@@ -8,18 +8,26 @@ SRC = "./src"
 
 def renderit(file)
   svg = Document.new(File.new("#{SRC}/#{file}", 'r'))
-  svg.root.each_element("//g[contains(@inkscape:label,'plate')]") do |icon|
-    context = icon.elements["text[@inkscape:label='context']/tspan"].text
-    icon_name = icon.elements["text[@inkscape:label='icon-name']/tspan"].text
-    puts "#{file}:#{icon.attributes['inkscape:label']}  #{context}/#{icon_name}"
-    icon.each_element("rect") do |box|
-      dir = "#{box.attributes['width']}x#{box.attributes['height']}/#{context}"
-      cmd = "#{INKSCAPE} -i #{box.attributes['id']} -e #{dir}/#{file.gsub(/\.svg$/,'.png')} #{SRC}/#{file} > /dev/null 2>&1"
-      File.makedirs(dir) unless File.exists?(dir)
-      system(cmd)
-      print "."
+  #puts "DEBUG: #{file}"
+  svg.root.each_element("//g[contains(@inkscape:label,'baseplate')]") do |icon|
+    if icon.attributes['inkscape:groupmode']=='layer' #only look inside layers, there may be pasted groups
+      context = icon.elements["text[@inkscape:label='context']/tspan"].nil? ? 'blank' : icon.elements["text[@inkscape:label='context']/tspan"].text
+      icon_name = icon.elements["text[@inkscape:label='icon-name']/tspan"].nil? ? 'blank' : icon.elements["text[@inkscape:label='icon-name']/tspan"].text
+      puts "#{file}:#{icon.attributes['inkscape:label']}  #{context}/#{icon_name}"
+      icon.each_element("rect") do |box|
+        dir = "#{box.attributes['width']}x#{box.attributes['height']}/#{context}"
+        out = "#{dir}/#{icon_name.gsub(/$/,'.png')}"
+        cmd = "#{INKSCAPE} -i #{box.attributes['id']} -e #{out} #{SRC}/#{file} > /dev/null 2>&1"
+        File.makedirs(dir) unless File.exists?(dir)
+        if File.exists?(out)
+          print "-" #skip if PNG exists
+        else
+          system(cmd)
+          print "."
+        end
+      end
+      puts ''
     end
-    puts ''
   end
 end
 
