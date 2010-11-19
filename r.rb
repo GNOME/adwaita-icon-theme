ee6ac00710c744f80df61cdffd57b206084cc69b
Lapo Calamandrei <calamandrei@gmail.com>
2010-11-19 13:37:25 +0100

    Med "disabled" color to 35% alpha istead of 25%

--- r.rb
+++ r.rb
@@ -5,7 +5,7 @@ require "fileutils"
 include REXML
 
 
-INKSCAPE = '/opt/artlibre/bin/inkscape'
+INKSCAPE = '/usr/bin/inkscape'
 #INKSCAPE = '/usr/bin/inkscape' # like this works for me, while using `which` inkscape hangs
 SRC = "src/gnome-stencils.svg"
 PREFIX = "gnome/scalable"
