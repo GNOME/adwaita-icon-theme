6596ef06644a0d7604ed2e1d88234c6d3474ccea
Jakub Steiner <jimmac@gmail.com>
2012-01-25 14:20:10 +0100

    filled box issues. bug #668647
    - don't use a path instead of the rect for the bounding box (lapo!!!)
    - instead of looking for a 16x16px rect, we now look for
      <15.95 to 16.94>

--- r.rb
+++ r.rb
@@ -24,7 +24,9 @@ def chopSVG(icon)
 		#system(cmd)
 		svgcrop = Document.new(File.new(icon[:file], 'r'))
 		svgcrop.root.each_element("//rect") do |rect| 
-			if rect.attributes["width"] == '16' && rect.attributes["height"] == '16'
+			w = ((rect.attributes["width"].to_f * 10).round / 10.0).to_i #get rid of 16 vs 15.99999 
+			h = ((rect.attributes["width"].to_f * 10).round / 10.0).to_i #Inkscape bugs
+			if w == 16 && h == 16
 				rect.remove
 			end
 		end
