3cd26ec77f775a547c458092ea9e68ff60edbd59
Chun-wei Fan <fanchunwei@src.gnome.org>
2017-03-13 15:54:21 +0800

    Visual Studio builds: Generate .pc files
    Generate the .pc files upon "nmake install"

---
+++ win32/adwaitapc.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+#
+# Utility script to generate .pc files for libsoup
+# for Visual Studio builds, to be used for
+# building introspection files
+
+# Author: Fan, Chun-wei
+# Date: March 10, 2016
+
+import os
+import sys
+
+from replace import replace_multi
+from pc_base import BasePCItems
+
+def main(argv):
+    base_pc = BasePCItems()
+
+    base_pc.setup(argv)
+
+    # Generate libsoup-2.4.pc
+    replace_multi(base_pc.top_srcdir + '/adwaita-icon-theme.pc.in',
+                  base_pc.srcdir + '/adwaita-icon-theme.pc',
+                  base_pc.base_replace_items)
+
+    # Generate libsoup-gnome-2.4.pc
+    replace_multi(base_pc.top_srcdir + '/gnome-icon-theme-symbolic.pc.in',
+                  base_pc.srcdir + '/gnome-icon-theme-symbolic.pc',
+                  base_pc.base_replace_items)
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
