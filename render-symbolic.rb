d4296e09d7092b24fc3766a51c4da57eee991f34
Sam Hewitt <sam@snwh.org>
2018-06-30 02:01:25 -0400

    Symbolic: tidying and improved rendering  - tidied up gnome-stencils; moved all icons within a larger canvas area  - deleted empty and unused elements from gnome-stencils  - all icons are now dark grey on white to reflect how they are seen in UI  - organization: moved some mislayered icons  - render script: now converts all strokes and objects to paths  - render script: included SVGO step for additional optimization (brings file size way down)

--- render-symbolic.rb
+++ render-symbolic.rb
@@ -6,22 +6,26 @@ include REXML
 
 
 INKSCAPE = 'flatpak run org.inkscape.Inkscape'
-# INKSCAPE = '/usr/bin/inkscape' # like this works for me, while using `which` inkscape hangs
+# INKSCAPE = '/usr/bin/inkscape'
 SRC = "src/symbolic/gnome-stencils.svg"
 PREFIX = "Adwaita/scalable"
+PREFIX32 = "Adwaita/scalable-up-to-32" # dirty but it allows rendering those up-to icons
+
+# SVGO is a Node.js SVG optimization tool install with 'sudo npm install -g svgo'
+# script will skip if SVGO is not present
+SVGO = '/usr/bin/svgo'
 
 def chopSVG(icon)
 	FileUtils.mkdir_p(icon[:dir]) unless File.exists?(icon[:dir])
 	unless (File.exists?(icon[:file]) && !icon[:forcerender])
 		FileUtils.cp(SRC,icon[:file]) 
 		puts " >> #{icon[:name]}"
-		cmd = "#{INKSCAPE} -f #{icon[:file]} --select #{icon[:id]} --verb=FitCanvasToSelection  --verb=EditInvertInAllLayers "
-		cmd += "--verb=EditDelete --verb=EditSelectAll --verb=SelectionUnGroup --verb=SelectionUnGroup --verb=SelectionUnGroup --verb=StrokeToPath --verb=FileVacuum "
-		cmd += "--verb=FileSave --verb=FileQuit > /dev/null 2>&1"
+		# extract the icon
+		cmd = "#{INKSCAPE} -f #{icon[:file]} "
+		cmd += "--select #{icon[:id]} --verb=FitCanvasToSelection --verb=EditInvertInAllLayers --verb=EditDelete " # delete everything but the icon
+		cmd += "--verb=FileVacuum --verb=FileSave --verb=FileQuit > /dev/null 2>&1"
 		system(cmd)
-		#saving as plain SVG gets rid of the classes :/
-		#cmd = "#{INKSCAPE} -f #{icon[:file]} -z --vacuum-defs -l #{icon[:file]} > /dev/null 2>&1"
-		#system(cmd)
+		# remove bounding rectangle
 		svgcrop = Document.new(File.new(icon[:file], 'r'))
 		svgcrop.root.each_element("//rect") do |rect| 
 			w = ((rect.attributes["width"].to_f * 10).round / 10.0).to_i #get rid of 16 vs 15.99999 
@@ -30,9 +34,19 @@ def chopSVG(icon)
 				rect.remove
 			end
 		end
-	icon_f = File.new(icon[:file],'w+')
-	icon_f.puts svgcrop
-	icon_f.close
+		icon_f = File.new(icon[:file],'w+')
+		icon_f.puts svgcrop
+		icon_f.close
+		# convert any strokes and objects to paths
+		cmd = "#{INKSCAPE} -f #{icon[:file]} --verb=EditSelectAll --verb=SelectionUnGroup --verb=SelectionUnGroup --verb=SelectionUnGroup --verb=ObjectToPath --verb=StrokeToPath "
+		cmd += "--verb=FileSave --verb=FileQuit > /dev/null 2>&1"
+		system(cmd)
+		# save as plain SVG
+		cmd = "#{INKSCAPE} -f #{icon[:file]} -z --vacuum-defs --export-plain-svg=#{icon[:file]} > /dev/null 2>&1"
+		system(cmd)
+		# remove as many extraneous elements as possible with SVGO
+		cmd = "#{SVGO} --pretty --disable=convertShapeToPath -i #{icon[:file]} -o  #{icon[:file]} > /dev/null 2>&1"
+		system(cmd)
 	else
 		puts " -- #{icon[:name]} already exists"
 	end
@@ -42,7 +56,7 @@ def get_output_filename(d,n)
 	if (/rtl$/.match(n))
 		outfile = "#{d}/#{n.chomp('-rtl')}-symbolic-rtl.svg"
 	else
-		outfile = "#{d}/#{n}-symbolic.svg"	  
+		outfile = "#{d}/#{n}-symbolic.svg"
 	end
 	return outfile
 end
@@ -62,10 +76,10 @@ if (ARGV[0].nil?) #render all SVGs
 			dir = "#{PREFIX}/#{context_name}"
 			icon_name = icon.attributes.get_attribute("inkscape:label").value
 			# prevent rendering of icons ending in :
-			if icon_name.end_with?("-alt", "-old", "-template", "-source", "-ltr", "-rtl", "-working")
-				puts " >> skipping icon '" + icon_name + "'"
+			if icon_name.end_with?("-alt", "-old", "-template", "-source", "-ltr", "-working")
+				puts " ++ skipping icon '" + icon_name + "'"
 			elsif icon_name =~ /\d$/
-				puts " >> skipping icon '" + icon_name + "'"
+				puts " ++ skipping icon '" + icon_name + "'"
 			else
 				chopSVG({ :name => icon_name,
 						:id => icon.attributes.get_attribute("id"),
@@ -78,8 +92,12 @@ if (ARGV[0].nil?) #render all SVGs
 else #only render the icons passed
 	icons = ARGV
 	ARGV.each do |icon_name|
-	icon = svg.root.elements["//g[@inkscape:label='#{icon_name}']"]
-	dir = "#{PREFIX}/#{icon.parent.attributes['inkscape:label']}"
+		icon = svg.root.elements["//g[@inkscape:label='#{icon_name}']"]
+		if icon_name == "process-working"
+			dir = "#{PREFIX32}/#{icon.parent.attributes['inkscape:label']}"
+		else
+			dir = "#{PREFIX}/#{icon.parent.attributes['inkscape:label']}"
+		end
 		chopSVG({	:name => icon_name,
 					:id => icon.attributes["id"],
 					:dir => dir,
