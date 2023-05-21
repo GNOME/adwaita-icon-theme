b3a526eddf3f7d093bad079d075355f3be3b3c46
Dan Yeaw <dan@yeaw.me>
2023-05-20 22:53:36 -0400

    Stop placeholders from generating

--- tools/find_svg_png_files.py
+++ tools/find_svg_png_files.py
@@ -5,6 +5,6 @@ from pathlib import Path
 
 src_dir = Path(__file__).resolve().parents[1]
 adwaita_dir = src_dir / "Adwaita"
-for f in adwaita_dir.rglob("*.[sp]*"):
+for f in adwaita_dir.rglob("*.svg") or adwaita_dir.rglob("*.png"):
     if not f.is_symlink():
         print(f.relative_to(adwaita_dir))
--- tools/find_symlinks.py
+++ tools/find_symlinks.py
@@ -5,6 +5,6 @@ from pathlib import Path
 
 src_dir = Path(__file__).resolve().parents[1]
 adwaita_dir = src_dir / "Adwaita"
-for f in adwaita_dir.rglob("*.[sp]*"):
+for f in adwaita_dir.rglob("*.svg") or adwaita_dir.rglob("*.png"):
     if f.is_symlink() and ".." not in str(f.readlink()):
         print(f.name, f.relative_to(adwaita_dir).parent, f.readlink())
