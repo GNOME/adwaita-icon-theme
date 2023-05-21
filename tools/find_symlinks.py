ad53a96774f6cf87dd7184d726ee3bcb670a4bd1
Dan Yeaw <dan@yeaw.me>
2023-05-20 22:55:33 -0400

    Allow relative symlink

--- tools/find_symlinks.py
+++ tools/find_symlinks.py
@@ -6,5 +6,5 @@ from pathlib import Path
 src_dir = Path(__file__).resolve().parents[1]
 adwaita_dir = src_dir / "Adwaita"
 for f in adwaita_dir.rglob("*.svg") or adwaita_dir.rglob("*.png"):
-    if f.is_symlink() and ".." not in str(f.readlink()):
+    if f.is_symlink():
         print(f.name, f.relative_to(adwaita_dir).parent, f.readlink())
