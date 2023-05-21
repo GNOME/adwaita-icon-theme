a1897875835c231aeac59331f57b7fd213b91df9
Dan Yeaw <dan@yeaw.me>
2023-05-21 15:12:14 -0400

    Fix symlink glob filtering out too many files

--- tools/find_cursors.py
+++ tools/find_cursors.py
@@ -6,6 +6,6 @@ from pathlib import Path
 src_dir = Path(__file__).resolve().parents[1]
 adwaita_dir = src_dir / "Adwaita"
 cursors_dir = adwaita_dir / "cursors"
-for f in cursors_dir.glob("*[!.cur]"):
-    if not f.is_symlink():
+for f in cursors_dir.glob("*"):
+    if f.suffix != ".cur" and not f.is_symlink():
         print(f.relative_to(adwaita_dir))
