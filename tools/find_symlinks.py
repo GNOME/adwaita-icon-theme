370da72bb259d8198a07ef692bc7479bad9b4700
Dan Yeaw <dan@yeaw.me>
2023-05-21 15:19:51 -0400

    Fix symlinks not installed for cursors

--- tools/find_symlinks.py
+++ tools/find_symlinks.py
@@ -8,3 +8,7 @@ adwaita_dir = src_dir / "Adwaita"
 for f in adwaita_dir.rglob("*.svg") or adwaita_dir.rglob("*.png"):
     if f.is_symlink():
         print(f.name, f.relative_to(adwaita_dir).parent, f.readlink())
+cursors_dir = adwaita_dir / "cursors"
+for f in cursors_dir.glob("*"):
+    if f.suffix != ".cur" and f.is_symlink():
+        print(f.name, f.relative_to(adwaita_dir).parent, f.readlink())
