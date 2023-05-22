d613e5a33d641e6d551db4982c36ae3ee36cf6ba
Jakub Steiner <jimmac@gmail.com>
2023-05-22 10:52:12 +0200

    build: don't skip the pngs
    - 16x16 were being ignored

--- tools/find_svg_png_files.py
+++ tools/find_svg_png_files.py
@@ -5,6 +5,7 @@ from pathlib import Path
 
 src_dir = Path(__file__).resolve().parents[1]
 adwaita_dir = src_dir / "Adwaita"
-for f in adwaita_dir.rglob("*.svg") or adwaita_dir.rglob("*.png"):
+icons = list(adwaita_dir.rglob("*.png")) + list(adwaita_dir.rglob("*.svg"))
+for f in icons:
     if not f.is_symlink():
         print(f.relative_to(adwaita_dir))
