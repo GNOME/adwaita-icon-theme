74e9b79471236320d2af4925d6c5bb7df22380ce
Jakub Steiner <jimmac@gmail.com>
2023-10-03 11:00:56 +0200

    cursors: only use gtk/css names
    - big cleanup of the cursor set. Only names defined by gtk/CSS:
      https://docs.gtk.org/gdk4/ctor.Cursor.new_from_name.html
      https://developer.mozilla.org/en-US/docs/Web/CSS/cursor
    - Mutter will fall back to `default` if a cursor isn't found. See
      https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/3295
    
      Fixes https://gitlab.gnome.org/GNOME/adwaita-icon-theme/-/issues/251

--- src/cursors/pngs/make-w32.sh
+++ src/cursors/pngs/make-w32.sh
@@ -13,63 +13,38 @@ do
   else
     s=
   fi
-  ${GEN} left_ptr_watch$s.in ../../../$theme/cursors/left_ptr_watch.ani
-  ${GEN} hand1$s.in ../../../$theme/cursors/hand1.cur
-  ${GEN} hand2$s.in ../../../$theme/cursors/hand2.cur
-  ${GEN} left_ptr$s.in ../../../$theme/cursors/left_ptr.cur
-#  ${GEN} center_ptr$s.in ../../../$theme/cursors/center_ptr.cur
-  ${GEN} xterm$s.in ../../../$theme/cursors/xterm.cur
-  ${GEN} crossed_circle$s.in ../../../$theme/cursors/crossed_circle.cur
-  ${GEN} right_ptr$s.in ../../../$theme/cursors/right_ptr.cur
+  ${GEN} default$s.in ../../../$theme/cursors/default.cur
+  ${GEN} help$s.in ../../../$theme/cursors/help.cur
+  ${GEN} pointer$s.in ../../../$theme/cursors/pointer.cur
+  ${GEN} context-menu$s.in ../../../$theme/cursors/context-menu.cur
+  ${GEN} progress$s.in ../../../$theme/cursors/progress.ani
+  ${GEN} wait$s.in ../../../$theme/cursors/wait.ani
+  ${GEN} cell$s.in ../../../$theme/cursors/cell.cur
+  ${GEN} crosshair$s.in ../../../$theme/cursors/crosshair.cur
+  ${GEN} text$s.in ../../../$theme/cursors/text.cur
+  ${GEN} vertical-text$s.in ../../../$theme/cursors/vertical-text.cur
+  ${GEN} alias$s.in ../../../$theme/cursors/alias.cur
   ${GEN} copy$s.in ../../../$theme/cursors/copy.cur
+  ${GEN} no-drop$s.in ../../../$theme/cursors/no-drop.cur
   ${GEN} move$s.in ../../../$theme/cursors/move.cur
-  ${GEN} pointer-move$s.in ../../../$theme/cursors/pointer-move.cur
-  ${GEN} link$s.in ../../../$theme/cursors/link.cur
-  ${GEN} sb_h_double_arrow$s.in ../../../$theme/cursors/sb_h_double_arrow.cur
-  ${GEN} sb_v_double_arrow$s.in ../../../$theme/cursors/sb_v_double_arrow.cur
-  ${GEN} top_left_corner$s.in ../../../$theme/cursors/top_left_corner.cur
-  ${GEN} top_right_corner$s.in ../../../$theme/cursors/top_right_corner.cur
-  ${GEN} bottom_left_corner$s.in ../../../$theme/cursors/bottom_left_corner.cur
-  ${GEN} bottom_right_corner$s.in ../../../$theme/cursors/bottom_right_corner.cur
-  ${GEN} fd_double_arrow$s.in ../../../$theme/cursors/fd_double_arrow.cur
-  ${GEN} bd_double_arrow$s.in ../../../$theme/cursors/bd_double_arrow.cur
-  ${GEN} watch$s.in ../../../$theme/cursors/watch.ani
-  ${GEN} sb_left_arrow$s.in ../../../$theme/cursors/sb_left_arrow.cur
-  ${GEN} sb_right_arrow$s.in ../../../$theme/cursors/sb_right_arrow.cur
-  ${GEN} sb_up_arrow$s.in ../../../$theme/cursors/sb_up_arrow.cur
-  ${GEN} sb_down_arrow$s.in ../../../$theme/cursors/sb_down_arrow.cur
-#  ${GEN} based_arrow_down$s.in ../../../$theme/cursors/based_arrow_down.cur
-#  ${GEN} based_arrow_up$s.in ../../../$theme/cursors/based_arrow_up.cur
-  ${GEN} bottom_side$s.in ../../../$theme/cursors/bottom_side.cur
-  ${GEN} top_side$s.in ../../../$theme/cursors/top_side.cur
-  ${GEN} left_side$s.in ../../../$theme/cursors/left_side.cur
-  ${GEN} right_side$s.in ../../../$theme/cursors/right_side.cur
+  ${GEN} not-allowed$s.in ../../../$theme/cursors/not-allowed.cur
+  ${GEN} grab$s.in ../../../$theme/cursors/grab.cur
   ${GEN} grabbing$s.in ../../../$theme/cursors/grabbing.cur
-  ${GEN} question_arrow$s.in ../../../$theme/cursors/question_arrow.cur
-  ${GEN} top_tee$s.in ../../../$theme/cursors/top_tee.cur
-  ${GEN} bottom_tee$s.in ../../../$theme/cursors/bottom_tee.cur
-  ${GEN} left_tee$s.in ../../../$theme/cursors/left_tee.cur
-  ${GEN} right_tee$s.in ../../../$theme/cursors/right_tee.cur
-  ${GEN} ul_angle$s.in ../../../$theme/cursors/ul_angle.cur
-  ${GEN} ll_angle$s.in ../../../$theme/cursors/ll_angle.cur
-  ${GEN} ur_angle$s.in ../../../$theme/cursors/ur_angle.cur
-  ${GEN} lr_angle$s.in ../../../$theme/cursors/lr_angle.cur
-  ${GEN} X_cursor$s.in ../../../$theme/cursors/X_cursor.cur
-  ${GEN} crosshair$s.in ../../../$theme/cursors/cell.cur
-  ${GEN} cross$s.in ../../../$theme/cursors/cross.cur
-  ${GEN} --no-shadows tcross$s.in ../../../$theme/cursors/tcross.cur
-  ${GEN} dotbox$s.in ../../../$theme/cursors/dotbox.cur
-  ${GEN} plus$s.in ../../../$theme/cursors/plus.cur
-  ${GEN} pencil$s.in ../../../$theme/cursors/pencil.cur
-  ${GEN} dnd-none$s.in ../../../$theme/cursors/dnd-none.cur
-  ${GEN} dnd-copy$s.in ../../../$theme/cursors/dnd-copy.cur
-  ${GEN} dnd-link$s.in ../../../$theme/cursors/dnd-link.cur
-  ${GEN} dnd-move$s.in ../../../$theme/cursors/dnd-move.cur
-  ${GEN} dnd-ask$s.in ../../../$theme/cursors/dnd-ask.cur
-  ${GEN} dnd-no-drop$s.in ../../../$theme/cursors/dnd-no-drop.cur
+  ${GEN} all-scroll$s.in ../../../$theme/cursors/all-scroll.cur
+  ${GEN} col-resize$s.in ../../../$theme/cursors/col-resize.cur
+  ${GEN} row-resize$s.in ../../../$theme/cursors/row-resize.cur
+  ${GEN} n-resize$s.in ../../../$theme/cursors/n-resize.cur
+  ${GEN} s-resize$s.in ../../../$theme/cursors/s-resize.cur
+  ${GEN} e-resize$s.in ../../../$theme/cursors/e-resize.cur
+  ${GEN} w-resize$s.in ../../../$theme/cursors/w-resize.cur
+  ${GEN} ne-resize$s.in ../../../$theme/cursors/ne-resize.cur
+  ${GEN} nw-resize$s.in ../../../$theme/cursors/nw-resize.cur
+  ${GEN} sw-resize$s.in ../../../$theme/cursors/sw-resize.cur
+  ${GEN} se-resize$s.in ../../../$theme/cursors/se-resize.cur
+  ${GEN} ew-resize$s.in ../../../$theme/cursors/ew-resize.cur
+  ${GEN} ns-resize$s.in ../../../$theme/cursors/ns-resize.cur
+  ${GEN} nesw-resize$s.in ../../../$theme/cursors/nesw-resize.cur
+  ${GEN} nwse-resize$s.in ../../../$theme/cursors/nwse-resize.cur
   ${GEN} zoom-in$s.in ../../../$theme/cursors/zoom-in.cur
   ${GEN} zoom-out$s.in ../../../$theme/cursors/zoom-out.cur
-  ${GEN} all-scroll$s.in ../../../$theme/cursors/all-scroll.cur
-  ${GEN} vertical-text$s.in ../../../$theme/cursors/vertical-text.cur
-  ${GEN} context-menu$s.in ../../../$theme/cursors/context-menu.cur
 done
